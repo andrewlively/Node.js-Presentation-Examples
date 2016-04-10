3dbd20e494052c0d0043eb61bfe0252a1f4ded4d
Andrew Lively <andrew.lively2@gmail.com>
2016-04-10 16:27:43 -0400

    Added body parser

--- web-applications/data-driven-app/src/index.js
+++ web-applications/data-driven-app/src/index.js
@@ -1,10 +1,15 @@
 const config = require(`../config.json`);
 const express = require(`express`);
 const app = express();
+const bodyParser = require(`body-parser`);
 
 app.set(`view engine`, `ejs`);
 app.set(`views`, `${ __dirname }/../views`);
 app.use(express.static(`${ __dirname }/../public`));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 app.use(`/`, require(`./routes/Index`));
 
--- web-applications/data-driven-app/src/routes/Index/index.js
+++ web-applications/data-driven-app/src/routes/Index/index.js
@@ -4,4 +4,8 @@ router.get(`/`, (req, res) => {
   res.render(`login`);
 });
 
+router.post(`/login`, (req, res) => {
+  
+});
+
 module.exports = router;
